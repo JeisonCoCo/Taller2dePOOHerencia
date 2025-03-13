f9f3bbeb206ad79e449befeddaadbb9552a9e607
Jeison Cordoba <jeisonsmit@gmail.com>
2025-03-12 21:33:35 -0500

    MetodoConstructor

--- 31/TriangularInferior/Ejercicio31.cs
+++ 31/TriangularInferior/Ejercicio31.cs
@@ -8,6 +8,25 @@ namespace TriangularInferior
 {
     public class Ejercicio31
     {
+        private int N;
+        private int[,] matriz;
+        public Ejercicio31(int n)
+        {
+            N = n;
+            matriz = new int[N ,N];
+        }
+        private void Ejercicio31()
+        {
+
+        }
         
+            for (int i = 0; i < _N; i++)
+            {
+            }
+            for (int j = 0; j < _N; j++)
+            {
+                matriz[i, j] = int + j;
+            }
+        }
     }
 }
